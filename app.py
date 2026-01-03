e910892bfe98a8dc90164bce0112f0ca57f8b937
Eeshansh <111873887+Eeshansh@users.noreply.github.com>
2026-01-03 07:50:31 +0000

    Updated

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
-import pandas as pd
-from flask import Flask
+from flask import Flask, request, jsonify
 import pickle
-from flask import request, jsonify
+import pandas as pd
 from features import extract_features
 
-
-
 app = Flask(__name__)
+
+# Load trained model
+with open("model/model.pkl", "rb") as f:
+    model = pickle.load(f)
 
 @app.route("/")
 def home():
     return "Phishing URL Detection API is running"
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-   
-with open("model/model.pkl", "rb") as f:
-    model = pickle.load(f)
-
+ 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
@@ -33,10 +27,13 @@ def predict():
 
     # Predict
     prediction = model.predict(features_df)[0]
-
     result = "Phishing" if prediction == 1 else "Legitimate"
 
     return jsonify({
         "url": url,
         "prediction": result
     })
+
+# 🔴 ALWAYS LAST
+if __name__ == "__main__":
+    app.run(debug=True)
